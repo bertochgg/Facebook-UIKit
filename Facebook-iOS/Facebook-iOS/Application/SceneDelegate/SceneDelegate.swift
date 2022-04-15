7353274bc2973980fe189c663dd07db4d0d7e23e
sliamtsev <sliamtsev@griddynamics.com>
2022-04-15 21:56:24 +0300

    Project structure + TODO txt files

--- Facebook-iOS/Facebook-iOS/Application/SceneDelegate/SceneDelegate.swift
+++ Facebook-iOS/Facebook-iOS/Application/SceneDelegate/SceneDelegate.swift
@@ -7,12 +7,31 @@
 
 import UIKit
 
-final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder {
 
+    // https://stackoverflow.com/questions/56588843/uiapplication-shared-delegate-equivalent-for-scenedelegate-xcode11
     var window: UIWindow?
+    
+    static var shared: SceneDelegate {
+        guard let sceneDelegate = UIApplication.shared.connectedScenes
+                .first?.delegate as? SceneDelegate else {
+                    preconditionFailure("Unable to get AppDelegate")
+                }
+        return sceneDelegate
+    }
+}
 
+// MARK: - UIWindowSceneDelegate
+extension SceneDelegate: UIWindowSceneDelegate {
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        let rootWindow = UIWindow(windowScene: windowScene)
+        window = rootWindow
+        rootWindow.rootViewController = SignInViewController()
+        rootWindow.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -34,5 +53,4 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidEnterBackground(_ scene: UIScene) {
         
     }
-
 }
---
+++ Facebook-iOS/Facebook-iOS/UserStory/SignInViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignInViewController.swift
+//  Facebook-iOS
+//
+//  Created by Serhii Liamtsev on 4/15/22.
+//
+
+import UIKit
+
+final class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        
+        view.backgroundColor = UIColor.systemBackground
+        
+        let label = UILabel()
+        label.text = "Hello World"
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+}
