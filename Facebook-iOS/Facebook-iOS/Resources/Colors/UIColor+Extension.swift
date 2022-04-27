57500f5941224d5240134d0f86c11947570c8813
sliamtsev <sliamtsev@griddynamics.com>
2022-04-27 11:29:46 +0300

    added TODO for colors, localization and fonts.

---
+++ Facebook-iOS/Facebook-iOS/Resources/Colors/UIColor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extension.swift
+//  Facebook-iOS
+//
+//  Created by Serhii Liamtsev on 4/27/22.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var facebookBlue: UIColor {
+        // #4267b2 = rgba(66,103,178,255)
+        return UIColor(red: 66.0 / 255.0, green: 103.0 / 255.0, blue: 178.0 / 255.0, alpha: 1.0)
+    }
+}
