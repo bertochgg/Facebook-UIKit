0183c542ece826b003bac3302f1e6eb41b4a7a5b
Humberto <hgarcia@griddynamics.com>
2023-05-30 11:25:38 -0600

    M6S21 - Changed FeedViewModel to handle feed and user data

--- Facebook-iOS/Facebook-iOS/UserStory/Main/Feed/FeedViewModel.swift
+++ Facebook-iOS/Facebook-iOS/UserStory/Main/Feed/FeedViewModel.swift
@@ -29,7 +29,22 @@ final class FeedViewModel: FeedViewModelProtocol {
                 self?.userProfileNetworkService.fetchProfileData { [weak self] result in
                     switch result {
                     case .success(let userProfileData):
-                        let viewModels = feedData.data.map { FeedTableViewCellViewModel(feedDatum: $0, userData: userProfileData) }
+                        let viewModels = feedData.data.map { feedDatum -> FeedTableViewCellViewModel in
+                            var imageURLs: [URL] = []
+                            if let attachments = feedDatum.attachments,
+                               let subattachments = attachments.data.first?.subattachments {
+                                imageURLs = subattachments.data.compactMap { $0.media.image?.src }
+                            }
+                            return FeedTableViewCellViewModel(
+                                id: UUID(),
+                                profileImageURL: userProfileData.picture.data.url,
+                                username: "\(userProfileData.firstName) \(userProfileData.lastName)",
+                                creationTime: feedDatum.createdTime.dateFormatting(),
+                                message: feedDatum.message,
+                                imageURLs: imageURLs,
+                                imageURL: feedDatum.attachments?.data.first?.media?.image?.src
+                            )
+                        }
                         self?.delegate?.didFetchFeedData(feedData: viewModels)
                     case .failure(let error):
                         print("Error fetching user profile data: \(error.localizedDescription)")
