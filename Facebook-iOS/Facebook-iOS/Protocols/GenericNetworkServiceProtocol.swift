454f08ec15b4fc55b84312d1145ff1e3ca7f1a25
Humberto <hgarcia@griddynamics.com>
2023-06-12 16:18:33 -0600

    M6S24 - Added again protocol file that was giving compilation problems

--- Facebook-iOS/Facebook-iOS/Protocols/GenericNetworkServiceProtocol.swift
+++ Facebook-iOS/Facebook-iOS/Protocols/GenericNetworkServiceProtocol.swift
@@ -1,30 +1,30 @@
 //
-// GenericNetworkServiceProtocol.swift
-// Facebook-iOS
+//  GenericNetworkServiceProtocol.swift
+//  Facebook-iOS
 //
-// Created by Humberto Garcia on 08/06/23.
+//  Created by Humberto Garcia on 08/06/23.
 //
 
 import Foundation
 
 protocol GenericNetworkServiceProtocol {
-  associatedtype DataModel: Decodable
-   
-  func parseJSON(json: Any, completion: @escaping (Result<DataModel, NetworkServiceErrors>) -> Void)
+    associatedtype DataModel: Decodable
+    
+    func parseJSON(json: Any, completion: @escaping (Result<DataModel, NetworkServiceErrors>) -> Void)
 }
 
 extension GenericNetworkServiceProtocol {
-  func parseJSON(json: Any, completion: @escaping (Result<DataModel, NetworkServiceErrors>) -> Void) {
-    do {
-      let jsonData = try JSONSerialization.data(withJSONObject: json)
-      let decoder = JSONDecoder()
-      decoder.keyDecodingStrategy = .useDefaultKeys
-      decoder.dateDecodingStrategy = .iso8601
-      let decodedData = try decoder.decode(DataModel.self, from: jsonData)
-      completion(.success(decodedData))
-    } catch let error {
-      completion(.failure(NetworkServiceErrors.decodingFailed))
-      print(error.localizedDescription)
+    func parseJSON(json: Any, completion: @escaping (Result<DataModel, NetworkServiceErrors>) -> Void) {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: json)
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .useDefaultKeys
+            decoder.dateDecodingStrategy = .iso8601
+            let decodedData = try decoder.decode(DataModel.self, from: jsonData)
+            completion(.success(decodedData))
+        } catch let error {
+            completion(.failure(NetworkServiceErrors.decodingFailed))
+            print(error.localizedDescription)
+        }
     }
-  }
 }
